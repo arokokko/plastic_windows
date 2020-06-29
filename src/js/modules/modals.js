9d1aea1595c698f205b31126a76d6ed4979b9b66
Aleks_Tihonov <arokokko@gmail.com>
2020-06-29 17:10:48 +0700

    Add function for opening modal .popup

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -28,6 +28,7 @@ const modals = () => {
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
 
 };
 
