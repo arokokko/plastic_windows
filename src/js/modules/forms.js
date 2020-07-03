de18a66646b3490f282fdd2fed5670e8c58e97ca
alex_tihonov <arokokko@gmail.com>
2020-07-03 18:28:00 +0700

    Create forms.js
    Create forms.js in dir modules (forms branch), do main functional
    for send forms, messages for customers and clear input then

---
+++ src/js/modules/forms.js
@@ -0,0 +1,56 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        input = document.querySelectorAll('input');
+
+    const message = {
+        loading: 'Идет загрузка...',
+        success: 'Сообщение отправлено. Скоро мы с вами свяжемся!',
+        failure: 'Сообщение не отправлено. Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInput = () => {
+        input.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+       item.addEventListener('submit', (e) => {
+           e.preventDefault();
+
+           let messageStatus = document.createElement('div');
+           messageStatus.classList.add('status');
+           item.appendChild(messageStatus);
+
+           const formData = new FormData(item);
+
+           postData('assets/server.php', formData)
+            .then(res => {
+                console.log(res);
+                document.querySelector('.status').textContent = message.success;
+            })
+            .catch(() => document.querySelector('.status').textContent = message.failure)
+            .finally(() => {
+                clearInput();
+                setTimeout(() => {
+                    messageStatus.remove();
+                }, 5000);
+            });
+
+       });
+
+    });
+
+};
+
+export default forms;
