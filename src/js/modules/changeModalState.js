9753bd419446ab4af48e283f2c6c24816b2054c9
alex_tihonov <arokokko@gmail.com>
2020-07-09 15:43:25 +0700

    Create checkInputNumber.js and import
    Create checkInputNumber.js - checking for input only numbers,
    import to changeModalState.js and call it for inputs width and height

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,9 +1,14 @@
+import checkInputNumber from './checkInputNumber';
+
 const changeModalState = (state) => {
     const windowForm = document.querySelectorAll('.balcon_icons_img'),
         windowWidth = document.querySelectorAll('#width'),
         windowHeight = document.querySelectorAll('#height'),
         windowType = document.querySelectorAll('#view_type'),
         windowProfile = document.querySelectorAll('.checkbox');
+
+    checkInputNumber('#width');
+    checkInputNumber('#height');
 
     function bindActionToElem(event, elem, prop) {
         elem.forEach((item, i) => {
---
+++ src/js/modules/checkInputNumber.js
@@ -0,0 +1,14 @@
+const checkInputNumber = (selector) => {
+    const numInputs = document.querySelectorAll(selector);
+
+        numInputs.forEach(item => {
+            item.addEventListener('input', () => {
+                item.value = item.value.replace(/\D/, '');
+            });
+        });
+
+};
+
+export default checkInputNumber;
+
+
