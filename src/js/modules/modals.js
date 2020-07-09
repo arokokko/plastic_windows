618ba96ddbe8660f7afcc4fea995f565b1fefd0a
alex_tihonov <arokokko@gmail.com>
2020-07-09 10:38:34 +0700

    Call 2 new func bindModal in modal.js
    Call function bindModal for .popup_calc_profile and for
    .popup_calc_end, both functions call with last argument false

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -51,7 +51,9 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     callModalByTime('.popup', 60000);
 
 };
