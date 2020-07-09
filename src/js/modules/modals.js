48b1f0f3d4f0b518f2cf3e95647395dd8cc52a83
alex_tihonov <arokokko@gmail.com>
2020-07-09 10:16:57 +0700

    Add data-attr index.html, change func bindModal
    Add data-attributes data-modal for all modals in index.html.
    Add new feature - close all modals by click to close or click to
    overlay.
    Change function bindModal in modal.js - add new argument -
    closeClickOverlay (true by default)

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,26 +1,41 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+			close = document.querySelector(closeSelector),
+			windows = document.querySelectorAll('[data-modal]');
 
         trigger.forEach((item) => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
-                }
+				}
+				
+				windows.forEach(item => {
+					item.style.display = 'none';
+				});
+
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
             });
         });
         
         close.addEventListener('click', () => {
+			windows.forEach(item => {
+				item.style.display = 'none';
+			});
+
             modal.style.display = 'none';
             document.body.style.overflow = '';
         });
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+
+				windows.forEach(item => {
+					item.style.display = 'none';
+				});
+
                 modal.style.display = 'none';
                 document.body.style.overflow = ''; 
             }
